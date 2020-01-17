085d6471e6657f6142b022be85ec00248a71cf21
yuulye <yuulye@gmail.com>
2020-01-17 21:27:48 +0700

    update

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends React.Component {
   componentDidMount() {
     $(function() {
       $.ajax({
-        url: "https://yuulye.herokuapp.com/data",
+        url: "http://localhost:5000/data",
         success: data => {
           let brackets = {
             teams : [],
@@ -23,10 +23,43 @@ class App extends React.Component {
             }
             console.log('round# ', teamCount);
             for (let i = 0; i < teamCount; i+=2) {
-              brackets.teams.push([`Team #${i+1}`, `Team #${i+2}`]);
+              brackets.teams.push([]);
+            }
+            let half = teamCount / 2;
+            let indexStart = 0;
+            let indexHalf = half;
+            for (let i = 0; i < half; i++) {
+              brackets.teams[i].push([`Spot #${indexStart+1}`]);
+              brackets.teams[i].push([`Spot #${indexHalf+1}`]);
+              indexStart++;
+              indexHalf++;
+            }
+          } else if (data.type === 'locked') {
+            const count = data.content.count;
+            const teams = data.content.teams;
+            let teamCount = 1;
+            for (; teamCount < count; teamCount*=2) {
+              console.log(teamCount, count);
+            }
+            console.log('round# ', teamCount);
+            for (let i = 0; i < teamCount; i+=2) {
+              brackets.teams.push([]);
+            }
+            let half = teamCount / 2;
+            let indexStart = 0;
+            let indexHalf = half;
+            for (let i = 0; i < half; i++) {
+              brackets.teams[i].push(teams[indexStart]);
+              if (!teams[indexHalf]) {
+                teams[indexHalf] = null;
+              } 
+              brackets.teams[i].push(teams[indexHalf]);
+              indexStart++;
+              indexHalf++;
             }
           }
           $('#bracket').bracket({
+            teamWidth: 128,
             init: brackets,
           });
         },
