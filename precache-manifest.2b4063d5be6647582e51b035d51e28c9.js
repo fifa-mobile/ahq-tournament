ece6f28e4dace0b77b7e5c233bc93f2cdc587730
yuulye <yuulye@gmail.com>
2020-01-17 19:46:53 +0700

    Updates

---
+++ precache-manifest.2b4063d5be6647582e51b035d51e28c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a80de89bec50f98aa721a8a89c5af247",
+    "url": "/ahq-tournament/index.html"
+  },
+  {
+    "revision": "d6f4defc923ae7452126",
+    "url": "/ahq-tournament/static/css/2.77169bba.chunk.css"
+  },
+  {
+    "revision": "5b3bf62d6173081a5d1e",
+    "url": "/ahq-tournament/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d6f4defc923ae7452126",
+    "url": "/ahq-tournament/static/js/2.2cb556a3.chunk.js"
+  },
+  {
+    "revision": "8de4bdbb127f280cc6b79279dd69f244",
+    "url": "/ahq-tournament/static/js/2.2cb556a3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5b3bf62d6173081a5d1e",
+    "url": "/ahq-tournament/static/js/main.3f81010a.chunk.js"
+  },
+  {
+    "revision": "4282e2091cb6ecc5eae8",
+    "url": "/ahq-tournament/static/js/runtime-main.8e48164f.js"
+  }
+]);
